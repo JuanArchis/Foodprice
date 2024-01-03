ce69fb1a42f64a13a42629c3a2c398461ee478cf
Juan Camilo Ordoñez Deñas <119546440+JuanArchis@users.noreply.github.com>
2024-01-03 15:11:34 -0500

    Update Paquete_Foodprice_ver_0_0_1.r

--- R/Paquete_Foodprice_ver_0_0_1.r
+++ R/Paquete_Foodprice_ver_0_0_1.r
@@ -160,6 +160,7 @@ if (self$Año>2022) {
       
 temp_dir_A <- tempdir()
 archivo_excel_A <- file.path(temp_dir_A, paste0("archivo_A_",self$Año, ".xlsx"))
+
 if (!exists("data_list_abast_ev")) {data_list_abast_ev <- new.env(parent = emptyenv())}
 nombre_data_abast <- paste0("data_list_abast_ev", self$Año)
 
@@ -167,8 +168,8 @@ nombre_data_abast <- paste0("data_list_abast_ev", self$Año)
 
 
   if (!file.exists(archivo_excel_A)) {
-  archivo_excel_A <- sprintf("https://www.dane.gov.co/files/operaciones/SIPSA/anex-SIPSAbastecimiento-Microdatos-%d.xlsx", self$Año)
-  download.file(archivo_excel_A, archivo_excel_A, mode = "wb",timeout = 444)
+  url_excel_A <- sprintf("https://www.dane.gov.co/files/operaciones/SIPSA/anex-SIPSAbastecimiento-Microdatos-%d.xlsx", self$Año)
+  download.file(url_excel_A, archivo_excel_A, mode = "wb",timeout = 444)
   assign(nombre_data_abast, rio::import_list(archivo_excel_A, setclass = "tbl"), envir = data_list_abast_ev)
     self$data_list_abas =get(nombre_data_abast, envir = data_list_abast_ev)
   } else {
@@ -183,8 +184,8 @@ nombre_data_abast <- paste0("data_list_abast_ev", self$Año)
   # Verificar si el archivo ya existe en el directorio temporal < 2023
 
   if (!file.exists(archivo_excel_A)) {
-  archivo_excel_A <- sprintf("https://www.dane.gov.co/files/operaciones/SIPSA/anex-SIPSAbastecimiento-Microdatos-%d.xlsx", self$Año)
-  download.file(archivo_excel_A, archivo_excel_A, mode = "wb",timeout = 444)
+  url_excel_A <- sprintf("https://www.dane.gov.co/files/operaciones/SIPSA/anex-SIPSAbastecimiento-Microdatos-%d.xlsx", self$Año)
+  download.file(url_excel_A, archivo_excel_A, mode = "wb",timeout = 444)
   assign(nombre_data_abast, rio::import_list(archivo_excel_A, setclass = "tbl"), envir = data_list_abast_ev)
     self$data_list_abas =get(nombre_data_abast, envir = data_list_abast_ev)
   } else {

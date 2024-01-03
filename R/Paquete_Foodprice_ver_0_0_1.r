9fd767d82c777716be1d92d0223268693826d64e
Juan Camilo Ordoñez Deñas <119546440+JuanArchis@users.noreply.github.com>
2024-01-03 11:47:47 -0500

    Update Paquete_Foodprice_ver_0_0_1.r

--- R/Paquete_Foodprice_ver_0_0_1.r
+++ R/Paquete_Foodprice_ver_0_0_1.r
@@ -89,7 +89,7 @@ public=list(
 
     Librerias=function(Librerias_base){
     Librerias_base = c("readxl","dplyr","ggplot2","reshape2","knitr","haven","foreign","stringi","labelled","tidyr","plyr","tidyverse",
-                        "lpSolve","Rglpk","Rsymphony","scatterplot3d","reshape","R6","rio","janitor") # Nombra las librerias necesarias
+                        "lpSolve","Rglpk","scatterplot3d","reshape","R6","rio","janitor") # Nombra las librerias necesarias
 
     if (!require("pacman")) install.packages("pacman") # Paquete que simplifica la carga de librerias
     pacman::p_load(char = Librerias_base);Librerias_base_print = paste0(paste0("'", Librerias_base, "'"), collapse = ", ") # Instala si es necesario, o en su defecto, sólo llama los paquetes
